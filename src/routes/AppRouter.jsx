fce648537dde5c7edb503fbaa00710f441dc95ec
Yasaswini <korrapatoyasaswini@gmail.com>
2024-12-11 18:39:02 +0530

    push

--- src/routes/AppRouter.jsx
+++ src/routes/AppRouter.jsx
@@ -1,7 +1,7 @@
 import React from "react";
 import { BrowserRouter as Router, Routes, Route } from "react-router-dom";
 import Home from "../pages/Home";
-import MainLayout from "../Layouts/MainLayout";
+import MainLayout from "../layouts/MainLayout";
 import Signup from "../pages/Signup";
 
 function AppRouter() {
